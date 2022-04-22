1a991a2cc56b2701611f81be1158dbcbec44e547
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-04-22 17:16:30 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Communicator from './Communicator';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<Communicator />, rootElement);
