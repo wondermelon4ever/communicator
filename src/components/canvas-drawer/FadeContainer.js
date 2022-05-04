31ec006c9a935a7367bb293ba921d198d779b663
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-04 18:06:24 +0900

    Create FadeContainer.js

---
+++ src/components/canvas-drawer/FadeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FadeContainer = (props) => {
+
+    React.useEffect(()=>{
+
+    }, []);
+
+    return (
+        <div id="fade"></div>
+    )
+}
+
+export default FadeContainer;
