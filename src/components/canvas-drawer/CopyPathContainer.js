4d20fbca000d69c14796ebd2c1d602d176be6a83
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-04 18:05:56 +0900

    Create CopyPathContainer.js

---
+++ src/components/canvas-drawer/CopyPathContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const CopyPathContainer = (props) => {
+
+    React.useEffect(()=>{
+
+    }, []);
+
+    return (
+        <section id="copy-container" className="context-popup">
+            <div>
+                <input type="checkbox" id="copy-last" checked onChange={ (e)=>console.log(e)}/>
+                <label htmlFor="copy-last">Copy last path</label>
+            </div>
+            <div style={{ marginTop: "5px" }}>
+                <input type="checkbox" id="copy-all" onChange={ (e)=>console.log(e)}/>
+                <label htmlFor="copy-all">Copy all paths</label>
+            </div>
+        </section>
+    )
+}
+
+export default CopyPathContainer;
