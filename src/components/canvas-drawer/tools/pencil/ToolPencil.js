5d49e631e842137416c015afa3f6c66e33434407
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-13 17:11:02 +0900

    Update ToolPencil.js

--- src/components/canvas-drawer/tools/pencil/ToolPencil.js
+++ src/components/canvas-drawer/tools/pencil/ToolPencil.js
@@ -3,18 +3,33 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import EditIcon from '@mui/icons-material/Edit';
+import { createStatusDispatcherSingleton, dispatch, MESSAGE_TYPES } from '../../common/StatusDispatcher';
 
 const ToolPencil = (props) => {
+
+    const [selected, setSelected] = React.useState(props.selected);
+
+    React.useEffect(()=>{
+        createStatusDispatcherSingleton().addListener(MESSAGE_TYPES.SELECTED_SHAPE, (messageType, message) => {
+            if(messageType === MESSAGE_TYPES.SELECTED_SHAPE && message !== 'pencil') setSelected(false);
+            else setSelected(true);
+        });
+    }, []);
     
     const handleOnClick = (e) => {
+        setSelected(true);
+        dispatch(MESSAGE_TYPES.SELECTED_SHAPE, "pencil");
+    }
 
+    const handleOnDoubleClick = (e) => {
+        console.log("double clicked !!");
     }
 
     return (
-        <div style={{ margine: "3px", padding: "3px" }}>
-            <Avatar alt="Pencil" sx={{ bgcolor: "#FFFFFF", width: 32, height: 32 }} variant="rounded">
+        <div style={{ display: props.show ? "block" : "none", margine: "3px", padding: "3px" }}>
+            <Avatar alt="Pencil" sx={{ bgcolor: selected ? "#f57f17" : "#FFFFFF", width: 32, height: 32 }} variant="rounded">
             <Tooltip title="Pencil">
-                <IconButton onClick={ handleOnClick }>
+                <IconButton onClick={ handleOnClick } onDoubleClick={ handleOnDoubleClick }>
                     <EditIcon fontSize="large" />
                 </IconButton>
             </Tooltip>
