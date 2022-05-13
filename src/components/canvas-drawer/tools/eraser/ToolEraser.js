3451c1cfc6e089fb498f393360f0cf112d2fa79a
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-13 17:08:46 +0900

    Update ToolEraser.js

--- src/components/canvas-drawer/tools/eraser/ToolEraser.js
+++ src/components/canvas-drawer/tools/eraser/ToolEraser.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import { blue, pink } from '@mui/material/colors';
 import SvgIcon from '@mui/material/SvgIcon';
+import { createStatusDispatcherSingleton, dispatch, MESSAGE_TYPES } from '../../common/StatusDispatcher';
 
 function EraserIcon (props) {
     return (
         <SvgIcon { ...props } >
-            <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z"
-            />
+            <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z" />
         </SvgIcon>
     );
 }
@@ -17,14 +16,24 @@ function EraserIcon (props) {
 
 const ToolEraser = (props) => {
     
-    const handleOnClick = (e) => {
+    const [selected, setSelected] = React.useState(props.selected);
 
+    React.useEffect(()=>{
+        createStatusDispatcherSingleton().addListener(MESSAGE_TYPES.SELECTED_SHAPE, (messageType, message) => {
+            if(messageType === MESSAGE_TYPES.SELECTED_SHAPE && message !== 'eraser') setSelected(false);
+            else setSelected(true);
+        });
+    }, []);
+    
+    const handleOnClick = (e) => {
+        setSelected(true);
+        dispatch(MESSAGE_TYPES.SELECTED_SHAPE, "eraser");
     }
 
     return (
         <div style={{ margine: "3px", padding: "3px" }}>
-            <Avatar alt="Pencil" sx={{ bgcolor: "#FFFFFF", width: 32, height: 32 }} variant="rounded">
-            <Tooltip title="Pencil">
+            <Avatar alt="Eraser" sx={{ bgcolor: selected ? "#f57f17" : "#FFFFFF", width: 32, height: 32 }} variant="rounded">
+            <Tooltip title="Eraser">
                 <IconButton onClick={ handleOnClick }>
                     <EraserIcon fontSize="large" />
                 </IconButton>
