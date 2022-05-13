0401b2994bb536821307c6638ed2c5efe3dc7292
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-13 17:12:34 +0900

    Update ToolText.js

--- src/components/canvas-drawer/tools/text/ToolText.js
+++ src/components/canvas-drawer/tools/text/ToolText.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
-
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import { blue, pink } from '@mui/material/colors';
+import { createStatusDispatcherSingleton, dispatch, MESSAGE_TYPES } from '../../common/StatusDispatcher';
 
 const ToolText = (props) => {
     
-    const handleOnClick = (e) => {
+    const [selected, setSelected] = React.useState(props.selected);
 
+    React.useEffect(()=>{
+        createStatusDispatcherSingleton().addListener(MESSAGE_TYPES.SELECTED_SHAPE, (messageType, message) => {
+            if(messageType === MESSAGE_TYPES.SELECTED_SHAPE && message !== 'text') setSelected(false);
+            else setSelected(true);
+        });
+    }, []);
+    
+    const handleOnClick = (e) => {
+        setSelected(true);
+        dispatch(MESSAGE_TYPES.SELECTED_SHAPE, "text");
     }
 
     return (
         <div style={{ margine: "3px", padding: "3px" }}>
-            <Avatar alt="Marker" sx={{ bgcolor: "#FFFFFF", width: 32, height: 32 }} variant="rounded">
-            <Tooltip title="Marker">
+            <Avatar alt="Text" sx={{ bgcolor: selected ? "#f57f17" : "#FFFFFF", width: 32, height: 32 }} variant="rounded">
+            <Tooltip title="Text">
                 <IconButton onClick={ handleOnClick }>
                     <TextFieldsIcon fontSize="large" />
                 </IconButton>
