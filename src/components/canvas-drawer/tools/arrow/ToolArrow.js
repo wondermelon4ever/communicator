715a62fdb12be2cab4ba937c17cc54dba1d9df38
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-13 17:07:42 +0900

    Create ToolArrow.js

---
+++ src/components/canvas-drawer/tools/arrow/ToolArrow.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { createStatusDispatcherSingleton, dispatch, MESSAGE_TYPES } from '../../common/StatusDispatcher';
+
+const ToolArrow = (props) => {
+    
+    const [selected, setSelected] = React.useState(props.selected);
+
+    React.useEffect(()=>{
+        createStatusDispatcherSingleton().addListener(MESSAGE_TYPES.SELECTED_SHAPE, (messageType, message) => {
+            if(messageType === MESSAGE_TYPES.SELECTED_SHAPE && message !== 'arrow') setSelected(false);
+            else setSelected(true);
+        });
+    }, []);
+    
+    const handleOnClick = (e) => {
+        setSelected(true);
+        dispatch(MESSAGE_TYPES.SELECTED_SHAPE, "arrow");
+    }
+
+    return (
+        <div style={{ display: props.show ? "block" : "none", margine: "3px", padding: "3px" }}>
+            <Avatar alt="Arrow" sx={{ bgcolor: selected ? "#f57f17" : "#FFFFFF", width: 32, height: 32 }} variant="rounded">
+            <Tooltip title="Arrow">
+                <IconButton onClick={ handleOnClick }>
+                    <ArrowUpwardIcon fontSize="large" />
+                </IconButton>
+            </Tooltip>
+            </Avatar>
+        </div>
+    )
+}
+
+export default ToolArrow;
