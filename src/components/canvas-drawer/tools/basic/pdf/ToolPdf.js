9ebed19f117e23c1527cf2042fb8da0cc2626b10
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-16 18:48:29 +0900

    Update ToolPdf.js

--- src/components/canvas-drawer/tools/basic/pdf/ToolPdf.js
+++ src/components/canvas-drawer/tools/basic/pdf/ToolPdf.js
@@ -4,17 +4,40 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import { createMeventDispatcherSingleton, dispatch, MEVENT_KINDS } from '../../../mevent/MeventDispatcher';
+import { createPdfHandlerSingleton } from './PdfHandler';
 
 const ToolPdf = (props) => {
     
     const [selected, setSelected] = React.useState(props.selected);
+    const [context, setContext] = React.useState(undefined);
+    const [handler, setHandler] = React.useState(undefined);
 
     React.useEffect(()=>{
-        createMeventDispatcherSingleton().addListener(MEVENT_KINDS.SELECTED_SHAPE, (mevent) => {
-            if(mevent.kind === MEVENT_KINDS.SELECTED_SHAPE && mevent.value.shape !== 'pdf') setSelected(false);
+        console.log("Tool pdf was loaded successfully.");
+    }, []);
+
+    React.useEffect(()=>{
+        if(context !== undefined) { 
+            setHandler(createPdfHandlerSingleton(context.mainContext, context.tempContext, selected));
+        }
+    }, [context]);
+
+    const addMeventListener = () => {
+        var dispatcher = createMeventDispatcherSingleton();
+        dispatcher.addListener(MEVENT_KINDS.CANVAS_INITED, (mevent) => {
+            setContext({
+                mainContext: mevent.value.mainContext,
+                tempContext: mevent.value.tempContext
+            });
+        });
+        
+        dispatcher.addListener(MEVENT_KINDS.SELECTED_SHAPE, (mevent) => {
+            if(mevent.value.shape !== 'pdf') setSelected(false);
             else setSelected(true);
         });
-    }, []);
+    }
+
+    addMeventListener();
     
     const handleOnClick = (e) => {
         setSelected(true);
@@ -27,10 +50,11 @@ const ToolPdf = (props) => {
                 shape: "pdf"
             }
         });
+        handler.onIconClicked();
     }
 
     return (
-        <div style={{ display: props.show ? "block" : "none", margine: "3px", padding: "3px" }}>
+        <div id="pdf-icon" style={{ display: props.show ? "block" : "none", margine: "3px", padding: "3px" }}>
             <Avatar alt="PDF" sx={{ bgcolor: selected ? "#f57f17" : "#FFFFFF", width: 32, height: 32 }} variant="rounded">
             <Tooltip title="PDF">
                 <IconButton onClick={ handleOnClick }>
