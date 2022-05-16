c2029f30a261be534e5ea4e69fd94fc903901be8
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-16 18:38:48 +0900

    Update ArrowHandler.js

--- src/components/canvas-drawer/tools/advanced/arrow/ArrowHandler.js
+++ src/components/canvas-drawer/tools/advanced/arrow/ArrowHandler.js
@@ -1,22 +1,49 @@
-import drawHelper, { setArrowHandler } from "../../DrawHelper";
+import { createMeventDispatcherSingleton, MEVENT_KINDS } from '../../../mevent/MeventDispatcher';
+import drawHelper from "../../DrawHelper";
+import ShapeHandler from '../../ShapeHandler';
+import { getPoints } from '../../../views/CanvasTemp';
 
 var arrowHandler = undefined;
-export default class ArrowHandler {
+export default class ArrowHandler extends ShapeHandler {
 
     constructor(context, tempContext) {
-        this.context = context;
-        this.tempContext = tempContext;
-        this.canvas = tempContext.canvas;
+        super(context, tempContext);
+
         this.ismousedown = false;
         this.prevX = 0;
         this.prevY = 0;
         this.arrowSize = 10;
+
+        this.addMeventListener();
         arrowHandler = this;
     }
 
-    mousedown = (e, points) => {
-        var x = e.pageX - this.canvas.offsetLeft,
-            y = e.pageY - this.canvas.offsetTop;
+    addMeventListener () {
+        var dispatcher = createMeventDispatcherSingleton();
+        dispatcher.addListener(MEVENT_KINDS.SELECTED_SHAPE, (mevent) => {
+            if(mevent.value.shape !== 'arrow') this.selected = false;
+            else this.selected = true;
+        });
+          
+        dispatcher.addListener(MEVENT_KINDS.MOUSE_DOWN, (mevent) => {
+            if(this.selected === false) return;
+            this.mousedown(mevent);
+        });
+    
+        dispatcher.addListener(MEVENT_KINDS.MOUSE_MOVE, (mevent) => {
+            if(this.selected === false || !this.ismousedown) return;
+            this.mousemove(mevent);
+        });
+    
+        dispatcher.addListener(MEVENT_KINDS.MOUSE_UP, (mevent) => {
+            if(this.selected === false) return;
+            this.mouseup(mevent);
+        });
+    }
+
+    mousedown = (mevent) => {
+        var x = mevent.wevt.pageX - this.canvas.offsetLeft,
+            y = mevent.wevt.pageY - this.canvas.offsetTop;
 
         var t = this;
 
@@ -24,42 +51,42 @@ export default class ArrowHandler {
         t.prevY = y;
 
         t.ismousedown = true;
+        drawHelper.redraw(this.context, this.tempContext, getPoints());
     }
 
-    mouseup = (e, points) => {
-        var x = e.pageX - this.canvas.offsetLeft,
-            y = e.pageY - this.canvas.offsetTop;
+    mouseup = (mevent) => {
+        var x = mevent.wevt.pageX - this.canvas.offsetLeft,
+            y = mevent.wevt.pageY - this.canvas.offsetTop;
 
+        var points = getPoints();
         var t = this;
         if (t.ismousedown) {
             points[points.length] = ['arrow', [t.prevX, t.prevY, x, y], drawHelper.getOptions()];
-
             t.ismousedown = false;
+            drawHelper.redraw(this.context, this.tempContext, points);
         }
     }
 
-    mousemove = (e, points) => {
-        var x = e.pageX - this.canvas.offsetLeft,
-            y = e.pageY - this.canvas.offsetTop;
+    mousemove = (mevent) => {
+        var x = mevent.wevt.pageX - this.canvas.offsetLeft,
+            y = mevent.wevt.pageY - this.canvas.offsetTop;
 
         var t = this;
         if (t.ismousedown) {
             this.tempContext.clearRect(0, 0, innerWidth, innerHeight);
-
             drawHelper.arrow(this.tempContext, [t.prevX, t.prevY, x, y]);
         }
     }
 }
 
-const createArrowHandler = (context, tempContext) => {
+const createArrowHandlerSingleton = (context, tempContext) => {
     if(arrowHandler === undefined) {
         arrowHandler = new ArrowHandler(context, tempContext);
-        setArrowHandler(arrowHandler);
     }
 
     return arrowHandler;
 }
 
 export {
-    createArrowHandler
+    createArrowHandlerSingleton
 }
