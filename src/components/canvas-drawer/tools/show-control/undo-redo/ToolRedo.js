23808e546f2f02d210aebd01c1b271840ab69979
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-17 16:26:49 +0900

    Update ToolRedo.js

--- src/components/canvas-drawer/tools/show-control/undo-redo/ToolRedo.js
+++ src/components/canvas-drawer/tools/show-control/undo-redo/ToolRedo.js
@@ -4,22 +4,23 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import RedoIcon from '@mui/icons-material/Redo';
 
-import { createRedoHandlerSingleton } from './RedoHandler';
+import { createUndoRedoHandlerSingleton } from './UndoRedoHandler';
 import { createMeventDispatcherSingleton, MEVENT_KINDS } from '../../../mevent/MeventDispatcher';
 
 const ToolRedo = (props) => {
 
+    const [existRedo, setExistRedo] = React.useState(false);
     const [selected, setSelected] = React.useState(props.selected);
     const [context, setContext] = React.useState(undefined);
     const [handler, setHandler] = React.useState(undefined);
 
     React.useEffect(()=>{
-        console.log("Tool redo was loaded successfully.");
+        console.log("Tool undoredo was loaded successfully.");
     }, []);
 
     React.useEffect(()=>{
         if(context !== undefined) { 
-            setHandler(createRedoHandlerSingleton(context.mainContext, context.tempContext, selected));
+            setHandler(createUndoRedoHandlerSingleton(context.mainContext, context.tempContext, selected));
         }
     }, [context]);
 
@@ -31,22 +32,34 @@ const ToolRedo = (props) => {
                 tempContext: mevent.value.tempContext
             });
         });
+
+        dispatcher.addListener(MEVENT_KINDS.NO_EXIST_REDO, (mevent) => {
+            setExistRedo(false);
+        });
+
+        dispatcher.addListener(MEVENT_KINDS.EXIST_REDO, (mevent) => {
+            setExistRedo(true);
+        });
     }
 
     addMeventListener();
     
     const handleOnClick = (e) => {
+        e.preventDefault();
         handler.redo(e);
     }
 
     return (
         <div style={{ display: props.show ? "block" : "none", margine: "3px", padding: "3px" }}>
-            <Avatar alt="Redo" sx={{ bgcolor: "#eeff41", width: 32, height: 32 }} variant="rounded">
+            <Avatar alt="Redo" sx={{ bgcolor: "#f0f4c3", width: 32, height: 32 }} variant="rounded">
             <Tooltip title="Redo">
-                <IconButton onClick={ handleOnClick }>
-                    <RedoIcon fontSize="large" />
+                <span>
+                <IconButton onClick={ handleOnClick } disabled={ existRedo ? false : true }>
+                    <RedoIcon fontSize="large" color={ existRedo ? "action" : "disabled" } />
                 </IconButton>
+                </span>
             </Tooltip>
+            
             </Avatar>
         </div>
     )
