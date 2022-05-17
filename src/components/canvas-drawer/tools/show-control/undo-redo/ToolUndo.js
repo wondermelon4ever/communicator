5f93526f1286312cf730578a08f25e227fd99c64
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-17 16:27:19 +0900

    Update ToolUndo.js

--- src/components/canvas-drawer/tools/show-control/undo-redo/ToolUndo.js
+++ src/components/canvas-drawer/tools/show-control/undo-redo/ToolUndo.js
@@ -4,22 +4,23 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import UndoIcon from '@mui/icons-material/Undo';
 
-import { createUndoHandlerSingleton } from './UndoHandler';
+import { createUndoRedoHandlerSingleton } from './UndoRedoHandler';
 import { createMeventDispatcherSingleton, MEVENT_KINDS } from '../../../mevent/MeventDispatcher';
 
 const ToolUndo = (props) => {
 
+    const [existUndo, setExistUndo] = React.useState(false);
     const [selected, setSelected] = React.useState(props.selected);
     const [context, setContext] = React.useState(undefined);
     const [handler, setHandler] = React.useState(undefined);
 
     React.useEffect(()=>{
-        console.log("Tool undo was loaded successfully.");
+        console.log("Tool undoredo was loaded successfully.");
     }, []);
 
     React.useEffect(()=>{
         if(context !== undefined) { 
-            setHandler(createUndoHandlerSingleton(context.mainContext, context.tempContext, selected));
+            setHandler(createUndoRedoHandlerSingleton(context.mainContext, context.tempContext, selected));
         }
     }, [context]);
 
@@ -31,21 +32,32 @@ const ToolUndo = (props) => {
                 tempContext: mevent.value.tempContext
             });
         });
+
+        dispatcher.addListener(MEVENT_KINDS.NO_EXIST_UNDO, (mevent) => {
+            setExistUndo(false);
+        });
+
+        dispatcher.addListener(MEVENT_KINDS.EXIST_UNDO, (mevent) => {
+            setExistUndo(true);
+        });
     }
 
     addMeventListener();
     
     const handleOnClick = (e) => {
+        e.preventDefault();
         handler.undo(e);
     }
 
     return (
         <div style={{ display: props.show ? "block" : "none", margine: "3px", padding: "3px" }}>
-            <Avatar alt="Undo" sx={{ bgcolor: "#eeff41", width: 32, height: 32 }} variant="rounded">
+            <Avatar alt="Undo" sx={{ bgcolor: "#f0f4c3", width: 32, height: 32 }} variant="rounded">
             <Tooltip title="Undo">
-                <IconButton onClick={ handleOnClick }>
-                    <UndoIcon fontSize="large" />
+                <span>
+                <IconButton onClick={ handleOnClick } disabled={ existUndo ? false : true }>
+                    <UndoIcon fontSize="large" color={ existUndo ? "action" : "disabled" } />
                 </IconButton>
+                </span>
             </Tooltip>
             </Avatar>
         </div>
