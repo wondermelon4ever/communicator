ee2df1fa6b1a98dea43dc110c5c1dd264ebba803
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-20 18:45:56 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Communicator from './Communicator';
-import CanvasDrawerWidget from './components/canvas-drawer/CanvasDrawerWidget';
+// import CanvasDrawerWidget from './components/canvas-drawer/CanvasDrawerWidget';
+import SvgTest from './components/canvas-drawer/SvgTerst';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<CanvasDrawerWidget />, rootElement);
+// ReactDOM.render(<CanvasDrawerWidget />, rootElement);
+ReactDOM.render(<SvgTest />, rootElement);
