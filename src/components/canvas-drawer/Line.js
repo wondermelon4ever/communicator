bd4809a04a28b94be1257ca1c3f01b03b1169f1b
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-20 18:47:26 +0900

    Create Line.js

---
+++ src/components/canvas-drawer/Line.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Line extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <>
+            </>
+        )
+    }
+}
