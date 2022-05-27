0b8d02cad473431b00ec5d120187f07d1b924a4a
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-27 18:44:18 +0900

    Update SvgCanvas.js

--- src/components/canvas-drawer/svgtest/SvgCanvas.js
+++ src/components/canvas-drawer/svgtest/SvgCanvas.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LineEdgeCircle from './LineEdgeCircle';
+import LineWithCircleVertex from './LineWithCircleVertex';
 
 const commands = [
     ["Line", [100, 100], [300, 100]]   
@@ -7,19 +8,20 @@ const commands = [
 const SvgCanvas = (props) => {
     
     return (
-        <div>
+        <>
             <div>
                 DEMO for SVG DRAWING
             </div>
-            <svg width="1000" height="1000">
-                <LineEdgeCircle 
-                    commands={ commands }
+            <svg width="1000" height="690">
+                <LineWithCircleVertex 
+                    lid="Line-1"
+                    command={ commands[0] }
                     stroke="black"
                     strokeWidth="1"
-                    fill="none"
+                    fill="black"
                 />
             </svg>
-        </div>
+        </>
     )
 }
 
