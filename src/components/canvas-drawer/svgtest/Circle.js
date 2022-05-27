d04f73204b82fd4e47b703a714f79552e37987e9
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-05-27 18:41:21 +0900

    Update Circle.js

--- src/components/canvas-drawer/svgtest/Circle.js
+++ src/components/canvas-drawer/svgtest/Circle.js
@@ -1,93 +1,119 @@
 import React from 'react';
-import { Menu, MenuItem } from '@mui/material';
+import { Divider, Menu, MenuItem } from '@mui/material';
 
-var num = 0;
-var handleMouseDrag = undefined;
-const Circle = (props) => {
+class Circle extends React.Component {
+    
+    static num = 0;
 
-    const [id, setId] = React.useState(props.id === undefined ? "circle-"+(num)++ : props.id);
-    const [position, setPosition] = React.useState(props.position);
-    const [radius, setRadius] =  React.useState(props.radius);
-    const [stroke, setStroke] = React.useState(props.stroke);
-    const [strokeWidth, setStrokeWidth] = React.useState(props.strokeWidth);
-    const [fill, setFill] = React.useState(props.fill);
+    constructor(props) {
+        super(props);
 
-    const [contextMenuOpen, setContextMenuOpen] = React.useState(false);
-    const [contextMenuAnchorPoint, setContextMenuAnchorPoint] = React.useState({ x: 10, y: 10 });
+        this.ismousedown = false;
+        
+        this.state = {
+            id: props.cid === undefined ? "circle-"+(Circle.num)++ : props.cid,
+            contextMenuOpen: false,
+            contextMenuAnchorPoint: { 
+                x: 10, y: 10
+            },
+            isTemp: props.isTemp === undefined ? false : props.isTemp
+        };
 
-    const [isTemp, setIsTemp] = React.useState(props.isTemp === undefined ? false : props.isTemp);
-    const [ismousedown, setIsmousedown] = React.useState(false);
+        this.handleContextMenuOpen = this.handleContextMenuOpen.bind(this);
+        this.handleContextMenuClose = this.handleContextMenuClose.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+    }
 
-    React.useEffect(()=>{
-        console.log("component circle was mounted :" + id);
-    }, []);
+    componentDidMount () {
+        console.log("component circle was mounted :" + this.state.id);
+    }
 
-    React.useEffect(()=>{
-        setPosition(props.position);
-    }, [props.position]);
-
-    const handleContextMenuOpen = (event) => {
+    handleContextMenuOpen = (event) => {
         event.preventDefault();
-        setContextMenuAnchorPoint({
+        var temp = this.state;
+        temp.contextMenuAnchorPoint = {
             x: event.pageX,
             y: event.pageY
-        });
-        setContextMenuOpen(true);
-        setIsmousedown(false);
+        };
+        temp.contextMenuOpen = true;
+        this.setState(temp);
+        this.ismousedown = false;
     }
 
-    const handleMouseDown = (event) => {
-        setIsmousedown(true);
-        handleMouseDrag = handleMouseMove;
-        document.addEventListener('mousemove', handleMouseDrag);
+    handleMouseDown = (event) => {
+        event.preventDefault();
+        this.ismousedown = true;
+        document.addEventListener('mousemove', this.handleMouseMove);
     }
 
-    const handleMouseUp = (event) => {
-        document.removeEventListener ('mousemove', handleMouseDrag, false);
-        setIsmousedown(false);
+    handleMouseUp = (event) => {
+        event.preventDefault();
+        this.ismousedown = false;
+        document.removeEventListener ('mousemove', this.handleMouseMove);
     }
 
-    const handleMouseMove = (event) => {
-        props.handlePositionChanged(props.cidx, {
+    handleMouseMove = (event) => {
+        event.preventDefault();
+        if(!this.ismousedown) return;
+        this.props.handlePositionChanged(this.props.cid, {
             x: event.offsetX,
             y: event.offsetY
-        });
-        setIsTemp(false);
+        }, false);
+        var temp = this.state;
+        temp.isTemp = false;
+        this.setState(temp);
     }
 
-    const handleContextMenuClose = () => {
-        setContextMenuOpen(false);
+    handleContextMenuClose = () => {
+        this.setState({
+            ...this.state,
+            contextMenuOpen: false
+        })
     }
 
-    return(
-        <>
-            <circle 
-                id={ id } 
-                cx={ position.x } 
-                cy={ position.y } 
-                r={ radius } 
-                stroke={ stroke } 
-                strokeWidth={ strokeWidth } 
-                fill={ isTemp === true ? "gray" : fill }
-                onContextMenu={ handleContextMenuOpen }
-                onMouseDown={ handleMouseDown }
-                onMouseUp={ handleMouseUp }
-            />
-            <Menu
-                open={ contextMenuOpen !== false }
-                onClose={ handleContextMenuClose }
-                anchorReference="anchorPosition"
-                anchorPosition={
-                    contextMenuOpen !== false
-                    ? { top: contextMenuAnchorPoint.y, left: contextMenuAnchorPoint.x }
-                    : undefined
-                }
-            >
-                <MenuItem onClick={ handleContextMenuClose }>Delete</MenuItem>
-                <MenuItem onClick={ handleContextMenuClose }>Delete</MenuItem>
-            </Menu>
-        </>
-    );
+    render () {
+        const { id, 
+                position, 
+                radius, 
+                stroke, 
+                strokeWidth, 
+                fill, 
+                isTemp, 
+                show } = this.props;
+        return(
+            <>
+                <circle 
+                    id={ id } 
+                    cx={ position.x }
+                    cy={ position.y }
+                    r={ radius } 
+                    stroke={ stroke } 
+                    strokeWidth={ strokeWidth } 
+                    fill={ isTemp === true ? "gray" : fill }
+                    onContextMenu={ this.handleContextMenuOpen }
+                    onMouseDown={ this.handleMouseDown }
+                    onMouseUp={ this.handleMouseUp }
+                    style={{ display: show ? "block" : "none" }}
+                    onClick={ this.props.toggleTempVertexShow }
+                />
+                <Menu
+                    open={ this.state.contextMenuOpen }
+                    onClose={ this.handleContextMenuClose }
+                    anchorReference="anchorPosition"
+                    anchorPosition={
+                        { top: this.state.contextMenuAnchorPoint.y, left: this.state.contextMenuAnchorPoint.x }
+                    }
+                >
+                    <MenuItem onClick={ this.handleContextMenuClose }>Curve</MenuItem>
+                    <MenuItem onClick={ this.handleContextMenuClose }>Straight</MenuItem>
+                    <Divider />
+                    <MenuItem onClick={ this.handleContextMenuClose }>Delete</MenuItem>
+                </Menu>
+            </>
+        );
+    }
 }
 
 export default Circle;
