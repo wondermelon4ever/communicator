5b637cfcf8f86035f9d67a415db0c81cdbb7a5ce
IN SOO PARK <68526615+wondermelon4ever@users.noreply.github.com>
2022-06-03 19:27:23 +0900

    Update LineWithCircleVertex.js

--- src/components/canvas-drawer/svgtest/LineWithCircleVertex.js
+++ src/components/canvas-drawer/svgtest/LineWithCircleVertex.js
@@ -1,98 +1,132 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { unmountComponentAtNode } from "react-dom";
 
 import Circle from './Circle';
-import Node, { STATE } from "./Node";
+import LinePath from './LinePath'
+import Node, { STATE } from "./Node"
 
 import { calculateTwoCenterPoints, createPath } from './LineDrawHelper';
-import LinePath from './LinePath';
 
 var num = 0;
 const radius = 4;
-const LineWithCircleVertex = ( { lid, command, stroke, strokeWidth, fill, ...props } ) => {
+export default class LineWithCircleVertex extends React.Component {
 
-    const [isMouseDown, setIsMouseDown] = React.useState(false);
-    const [anchorPosition, setAnchorPosition] = React.useState({
-        x: 100,
-        y: 100
-    })
-    const [isMouseOver, setIsMouseOver] = React.useState(false);
+    static nodeNum = 0;
+    constructor(props) {
+        super(props);
 
-    // const [lineId, setLineId] = useState(lid ? lineId : "LineWithCircleEdge-${num++}");
-    const [lineId, setLineId] = React.useState(lid ? lid : "LineWithCircleEdge-"+(num)++);
+        this.head = undefined, this.tail = undefined;
+        this.childLeftTopMost = {
+            left: 0, top: 0
+        }
 
-    const [head, setHead] = useState(undefined);
-    const [tail, setTail] = useState(undefined);
-    const [pathList, setPathList] = useState([]);
+        this.prevMousePosition = { x: 0, y: 0 };
 
-    const [vertexList, setVertexList] = useState([]);
-    const [showTempVertex, setShowTempVertex] = useState(false);
-    // const [lineType, setLineType] = useState("Straight");
-    const [lineType, setLineType] = useState("Curve");
+        this.state = {
+            lineId: this.props.lid ? this.props.lid : "LineWithCircleEdge-"+(num)++,
+            isMouseDown: false,
+            anchorPosition: {
+                x: 0, y: 0
+            },
+            isMouseOver: false,
 
-    const nodeNum = useRef(0);
+            pathList: [],
+            vertexList: [],
+            showTempVertex: false,
+            lineKind: "Curve",
+            lineNum: 0
+        }
 
-    const handleMove = useRef(function (event) {
-        console.log("handle move !!!");
-        setAnchorPosition({
-            // x: event.offsetX,
-            // y: event.offsetY
-        })
-    });
+        this.draw = this.draw.bind(this);
+        this.handleOnVertexPositionChanged = this.handleOnVertexPositionChanged.bind(this);
+        this.toggleTempVertexShow = this.toggleTempVertexShow.bind(this);
 
-    useEffect(()=>{
+        this.handleOnMouseDown = this.handleOnMouseDown.bind(this);
+        this.handleOnMouseUp = this.handleOnMouseUp.bind(this);
+        this.handleOnMouseMove = this.handleOnMouseMove.bind(this);
+        this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
+        this.handleOnMouseLeave =  this.handleOnMouseLeave.bind(this);
+    }
+
+    componentDidMount () {
+        var command = this.props.command;
         var cmd = command[0];
         if(cmd !== "Line") {
             unmountComponentAtNode(document.getElementById("root"));
-        } else {
-            const pos1 = { x: command[1][0], y: command[1][1] }, pos2 = { x: command[2][0], y: command[2][1] };
-            var h = new Node(nodeNum.current++, pos1), t = new Node(nodeNum.current++, pos2);
-            h.setState(STATE.DRAWABLE), t.setState(STATE.DRAWABLE);
-
-            var temp = new Node(nodeNum.current++, {
-                x: (pos1.x+pos2.x)/2,
-                y: (pos1.y+pos2.y)/2
-            });
-            temp.setState(STATE.TEMP);
-            temp.setRadius(3);
-
-            h.append(temp); 
-            h.append(t);
-            setHead(h);
-            setTail(t);
+            return;
         }
-        var element = document.getElementById(lineId);
-        var rect = element.getBoundingClientRect();
-        console.log("position of this line group=>" + JSON.stringify(rect));
-    }, []);
+        const pos1 = { x: command[1][0], y: command[1][1] }, pos2 = { x: command[2][0], y: command[2][1] };
 
-    useEffect(()=>{
-        draw(true);
-    }, [head]);
+        if(pos1.x < pos2.x) this.childLeftTopMost.left = pos1.x;
+        else this.childLeftTopMost.left = pos2.x;
+        if(pos1.y < pos2.y) this.childLeftTopMost.top = pos1.y;
+        else this.childLeftTopMost.top = pos2.y;
 
-    const draw = (isInit) => {
-        if(head === undefined) return;
+        var h = new Node(LineWithCircleVertex.nodeNum++, pos1), t = new Node(LineWithCircleVertex.nodeNum++, pos2);
+        h.setState(STATE.DRAWABLE), t.setState(STATE.DRAWABLE);
 
-        var node = head, prev, next, vList = [], pList = [];
+        var temp = new Node(LineWithCircleVertex.nodeNum++, {
+            x: (pos1.x+pos2.x)/2,
+            y: (pos1.y+pos2.y)/2
+        });
+        temp.setState(STATE.TEMP);
+        temp.setRadius(3);
+
+        h.append(temp); 
+        h.append(t);
+
+        this.head = h;
+        this.draw(true);
+    }
+
+    draw (isInit) { 
+        if(this.head === undefined) return;
+
+        var node = this.head, prev, next, vList = [], pList = [];
         var path = "";
         if(isInit) {
             prev = node, node = prev.next, next = node.next;
             path += "M"+prev.position.x+","+prev.position.y+" ";
-            path += lineType==="Straight" ? "L" : "Q";
+            path += this.state.lineType==="Straight" ? "L" : "Q";
             path += node.position.x+","+node.position.y+","+next.position.x+","+next.position.y;
-            pList.push(path);
+            // pList.push(path);
+
+            pList.push({
+                id: this.state.lineId + "-" + this.state.lineNum++,
+                point1: prev.position,
+                point2: next.position,
+                pointc: node.position,
+                lineKind: this.state.lineKind,
+                values: {
+                    stroke: this.props.stroke,
+                    strokeWidth: this.props.strokeWidth,
+                    fill: this.props.fill
+                }
+            });
 
             vList.push({ id: prev.id, position: prev.position, fill: prev.color, radius: radius, isTemp: false });
             vList.push({ id: node.id, position: node.position, fill: node.color, radius: radius, isTemp: true  });
             vList.push({ id: next.id, position: next.position, fill: next.color, radius: radius, isTemp: false });
         } else { 
-            prev = head, node = prev.next, next = node.next;
+            prev = this.head, node = prev.next, next = node.next;
             // 0번 Vertex 저장
             vList.push({ id: prev.id, position: prev.position, fill: prev.color, radius: radius, isTemp: prev.state === STATE.TEMP ? true : false });
 
             while(node !== undefined) {
-                path += createPath(prev.position, node.controlPosition, next.position, lineType);
-                pList.push(path);
+                // path += createPath(prev.position, node.controlPosition, next.position, this.state.lineType);
+                // pList.push(path);
+                pList.push({
+                    id: this.state.lineId + "-" + this.state.lineNum++,
+                    point1: prev.position,
+                    point2: next.position,
+                    pointc: node.controlPosition,
+                    lineKind: this.state.lineKind,
+                    values: {
+                        stroke: this.props.stroke,
+                        strokeWidth: this.props.strokeWidth,
+                        fill: this.props.fill
+                    }
+                });
                 // This always should be temp node
                 vList.push({
                     id: node.id,
@@ -114,13 +148,19 @@ const LineWithCircleVertex = ( { lid, command, stroke, strokeWidth, fill, ...pro
                 if(node !== undefined) next = node.next;
             }
         }
-        setVertexList(vList);
-        setPathList(pList);
+        this.setState({
+            ...this.state,
+            vertexList: vList,
+            pathList: pList
+        })
     }
 
-    const handleVertexPositionChanged = (vid, position) => {
+    handleOnVertexPositionChanged (vid, position) {
         if(position === undefined) return;
-        var h = head;
+        if(position.x < this.childLeftTopMost.left) this.childLeftTopMost.left = position.x;
+        if(position.y < this.childLeftTopMost.top)  this.childLeftTopMost.top  = position.y;
+
+        var h = this.head;
         var node = h.find(vid);
         node.position = position;
         if(node.state === STATE.TEMP) {
@@ -128,24 +168,24 @@ const LineWithCircleVertex = ( { lid, command, stroke, strokeWidth, fill, ...pro
             node.color = "black";
             node.setRadius(4);
             var prev = node.prev, next = node.next;
-            const centerPoints = calculateTwoCenterPoints(prev.position, node.position, next.position, lineType);
+            const centerPoints = calculateTwoCenterPoints(prev.position, node.position, next.position, this.state.lineKind);
             const tpos1 = centerPoints[0], tpos2 = centerPoints[1];
             
-            var temp1 = new Node(nodeNum.current++, { x: tpos1.x, y: tpos1.y });
+            var temp1 = new Node(LineWithCircleVertex.nodeNum++, { x: tpos1.x, y: tpos1.y });
             temp1.setColor("gray");
             temp1.setRadius(3);
             temp1.setState(STATE.TEMP);
             temp1.setControlPosition({ x: tpos1.cx, y: tpos1.cy });
             prev.insert(temp1);
 
-            var temp2 = new Node(nodeNum.current++, { x: tpos2.x, y: tpos2.y });
+            var temp2 = new Node(LineWithCircleVertex.nodeNum++, { x: tpos2.x, y: tpos2.y });
             temp2.setColor("gray");
             temp2.setRadius(3);
             temp2.setState(STATE.TEMP);
             temp2.setControlPosition({ x: tpos2.cx, y: tpos2.cy });
             node.insert(temp2);
         } else {
-            if(nodeNum.current < 4) {
+            if(LineWithCircleVertex.nodeNum < 4) {
                 // 초기유지상태임: 기본노드 2개, 임시노드 1개 
                 var temp;
                 if(node.prev === undefined) temp = node.next;
@@ -161,7 +201,7 @@ const LineWithCircleVertex = ( { lid, command, stroke, strokeWidth, fill, ...pro
                 else if(node.next === undefined) rnode = node, cnode = rnode.prev.prev, lnode = cnode.prev.prev;
                 else cnode = node, lnode = cnode.prev.prev, rnode = cnode.next.next;
                 
-                const centerPoints = calculateTwoCenterPoints(lnode.position, cnode.position, rnode.position, lineType);
+                const centerPoints = calculateTwoCenterPoints(lnode.position, cnode.position, rnode.position, this.state.lineKind);
                 var ltemp = cnode.prev, rtemp = cnode.next;
                 ltemp.position = { x: centerPoints[0].x, y: centerPoints[0].y };
                 ltemp.controlPosition = { x: centerPoints[0].cx, y: centerPoints[0].cy };
@@ -169,108 +209,128 @@ const LineWithCircleVertex = ( { lid, command, stroke, strokeWidth, fill, ...pro
                 rtemp.controlPosition = { x: centerPoints[1].cx, y: centerPoints[1].cy };
             }
         }
-        draw(false);
+        this.draw(false);
     }
 
-    const toggleTempVertexShow = (event) => {
-        setShowTempVertex(!showTempVertex);
+    toggleTempVertexShow (event) {
+        this.setState({
+            ...this.state,
+            showTempVertex: !this.state.showTempVertex
+        })
     }
 
-    // const handleMove = (event) => {
-    //     console.log("handle move !!!");
-    //     setAnchorPosition({
-    //         x: event.offsetX,
-    //         y: event.offsetY
-    //     })
-    // }
-
-    const handleMouseDown = (event) => {
+    handleOnMouseMove (event) {
         event.preventDefault();
-        console.log("handle mouse down ### !!!");
-        document.addEventListener("mousemove", handleMove.current);
-        setIsMouseDown(true);
+        if(event.ctrlKey === false || this.state.isMouseDown === false) return;
+        
+        this.setState({
+            ...this.state,
+            anchorPosition: {
+                x: this.state.anchorPosition.x+(event.clientX-this.prevMousePosition.x),
+                y: this.state.anchorPosition.y+(event.clientY-this.prevMousePosition.y)
+            }
+        })
+        this.prevMousePosition = { x: event.clientX, y: event.clientY };
     }
 
-    const handleMouseUp = (event) => {
+    handleOnMouseDown (event) {
         event.preventDefault();
-        console.log("handle mouse up ### !!!");
-        document.removeEventListener("mousemove", handleMove.current);
-        setIsMouseDown(false);
+        this.prevMousePosition = {
+            x: event.clientX, y: event.clientY
+        }
+
+        document.addEventListener("mousemove", this.handleOnMouseMove);
+        this.setState({
+            ...this.state,
+            isMouseDown: true
+        })
     }
 
-    const handleMouseOver = (event) => {
-        // setIsMouseOver(true);
+    handleOnMouseUp = (event) => {
+        event.preventDefault();
+        document.removeEventListener("mousemove", this.handleOnMouseMove);
+        this.prevMousePosition = { x: 0, y : 0 };
+        this.setState({
+            ...this.state,
+            isMouseDown: false
+        });
     }
 
-    const handleMouseLeave = (event) => {
-        // setIsMouseOver(false);
+    handleOnMouseOver (event) {
+        this.setState({
+            ...this.state,
+            isMouseOver: true
+        })
     }
 
-    return (
-        <svg id={ lineId }
-            // onMouseDown={ handleMouseDown }
-            // onMouseUp={ handleMouseUp }
-            // onMouseOver={ handleMouseOver }
-            // onMouseLeave= { handleMouseLeave }
-            // pointerEvents="bounding-box"
-            // style={{
-            //     cursor: isMouseOver ? "move" : "pointer",
-            // }}
-            // y={anchorPosition.y}
-            // x={anchorPosition.x}
-        >
-            {
-                pathList.map((path, index)=>{
-                    return (
-                        <path 
-                            key={ index }
-                            d={ path } 
-                            stroke={ stroke } 
-                            strokeWidth={ strokeWidth } 
-                            fill="none" 
-                            onClick={ toggleTempVertexShow }
-                            
-                        />
-                    )
-                })
-            }
-            {
-                vertexList.map((node, index)=>{
-                    return (
-                        <Circle 
-                            key={ node.id }
-                            cid={ node.id }
-                            position={ node.position }
-                            radius={ node.radius }
-                            stroke={ stroke} 
-                            strokeWidth={ strokeWidth } 
-                            fill={ node.fill } 
-                            isTemp={ node.isTemp }
-                            handlePositionChanged={ handleVertexPositionChanged }
-                            show={ node.isTemp === false || showTempVertex ? true : false }
-                            toggleTempVertexShow={ toggleTempVertexShow }
-                        />
-                    )
-                })
-            }
-            <LinePath 
-                lineKind="Curve"
-                point1={{ x: 200, y: 200 }}
-                point2={{ x: 400, y: 100 }}
-                pointc={{ x: 200, y: 100 }}
-                values={{
-                    stroke: "red",
-                    strokeWidth: "2",
-                    fill: "none",
+    handleOnMouseLeave (event) {
+        this.setState({
+            ...this.state,
+            isMouseOver: false
+        })
+    }
+
+    handleOnLinePathClicked (event) {
+        console.log("handle on line path click !!!");
+    }
+
+    render () {
+        return(
+            <svg id={ this.state.lineId }
+                onMouseDown={ this.handleOnMouseDown }
+                onMouseUp={ this.handleOnMouseUp }
+                onMouseOver={ this.handleOnMouseOver }
+                onMouseLeave= { this.handleOnMouseLeave }
+                pointerEvents="bounding-box"
+                style={{
+                    cursor: this.state.isMouseOver ? "move" : "pointer",
+                    zIndex: 0
                 }}
-                handleOnLinePathClick={ () => console.log("handleon line path clicked !!!")}
-                anchor={{
-                    x: anchorPosition.x,
-                    y: anchorPosition.y
-                }}
-            />
-        </svg>
-    )
+                y={ this.state.anchorPosition.y}
+                x={ this.state.anchorPosition.x}
+            >
+                {
+                    this.state.pathList.map((line, index)=>{
+                        return (
+                            <LinePath 
+                                key={ line. id }
+                                id={ line.id }
+                                point1={ line.point1 }
+                                point2={ line.point2 }
+                                pointc={ line.pointc }
+                                values={{
+                                    stroke: line.values.stroke,
+                                    strokeWidth: line.values.strokeWidth,
+                                    fill: line.values.fill
+                                }}
+                                lineKind={ line.lineKind }
+                                handleOnLinePathClick={ this.handleOnLinePathClicked }
+                                offset={ this.state.anchorPosition }
+                            />
+                        )
+                    })
+                }
+                {
+                    this.state.vertexList.map((node, index)=>{
+                        return (
+                            <Circle 
+                                key={ node.id }
+                                cid={ node.id }
+                                position={ node.position }
+                                radius={ node.radius }
+                                stroke={ this.props.stroke} 
+                                strokeWidth={ this.props.strokeWidth } 
+                                fill={ node.fill } 
+                                isTemp={ node.isTemp }
+                                handlePositionChanged={ this.handleOnVertexPositionChanged }
+                                show={ node.isTemp === false || this.state.showTempVertex ? true : false }
+                                toggleTempVertexShow={ this.toggleTempVertexShow }
+                                offset={ this.state.anchorPosition }
+                            />
+                        )
+                    })
+                }
+            </svg>
+        )
+    }
 }
-
-export default LineWithCircleVertex;
